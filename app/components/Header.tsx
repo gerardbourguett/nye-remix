46e538d3aac515f43c3e38007458e8f3e8a62078
Gerard Bourguett <gerard.bourguett@gmail.com>
2025-01-17 21:49:19 -0300

    Refactor componente Header para eliminar la animación y mostrar el año actual de forma estática

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,18 +1,9 @@
 import { CURRENT_YEAR } from "~/data/constants";
-import { animate, motion, useMotionValue } from "motion/react";
-import { useEffect } from "react";
 
 const Header = () => {
-  const count = useMotionValue(2000);
-
-  useEffect(() => {
-    const controls = animate(count, CURRENT_YEAR + 1, { duration: 4 });
-    return () => controls.stop();
-  }, []);
-
   return (
     <h1 className="text-6xl sm:text-8xl md:text-9xl font-medium tracking-tighter dark:text-slate-200 text-slate-800">
-      #<motion.span>{Math.round(count.get())}</motion.span>
+      #{CURRENT_YEAR + 1}
       <span className="text-blue-700 animate-pulse">Live</span>
     </h1>
   );
